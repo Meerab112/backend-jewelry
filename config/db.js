5900a69293bf130db3ea525e4eed58abd2ba6a95
meernaseer <laraibq338@gmail.com>
2026-06-17 02:06:41 +0500

    fix: use env variables for DB connection

--- config/db.js
+++ config/db.js
@@ -1,32 +1,14 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "jewlery",
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "jewlery",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
 module.exports = db;
-/*const mysql = require("mysql2");
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "jewlery",
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("DB connection failed:", err);
-  } else {
-    console.log("MySQL Connected...");
-  }
-});
-
-module.exports = db;
-*/
